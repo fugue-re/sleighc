16b0f0c2e20e7694612a0fd87b8853c1991db79c
Sam L. Thomas <s@ghost.sh>
2025-07-28 01:49:04 +0100

    add flake.nix

---
+++ flake.nix
@@ -0,0 +1,57 @@
+{
+  description = "Ghidra's SLEIGH compiler";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+  outputs =
+    {
+      self,
+      nixpkgs,
+    }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      requiredPkgs =
+        pkgs: with pkgs; [
+          bison
+          cmake
+          flex
+          libbfd
+          zlib
+        ];
+    in
+    {
+      packages = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs { system = system; };
+        in
+        {
+          default = pkgs.stdenv.mkDerivation {
+            name = "sleighc";
+            version = "11.3";
+            src = self;
+            buildInputs = requiredPkgs pkgs;
+          };
+        }
+      );
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs { system = system; };
+        in
+        {
+          default = pkgs.mkShell {
+            name = "sleighc-shell";
+            buildInputs = requiredPkgs pkgs;
+            shellHook = ''
+              export PATH=${self.packages.${system}.default}/bin:$PATH
+            '';
+          };
+        }
+      );
+    };
+}
